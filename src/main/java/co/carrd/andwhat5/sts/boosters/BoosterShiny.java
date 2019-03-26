74f9d602c98cf46be483898af760d0257a8e9417
sud0gh0st <46177533+sud0gh0st@users.noreply.github.com>
2019-03-26 21:51:57 +0000

    Update BoosterShiny.java

--- src/main/java/co/carrd/andwhat5/sts/boosters/BoosterShiny.java
+++ src/main/java/co/carrd/andwhat5/sts/boosters/BoosterShiny.java
@@ -2,7 +2,7 @@ package co.carrd.andwhat5.sts.boosters;
 
 import co.carrd.andwhat5.sts.config.STSConfig;
 import co.carrd.andwhat5.sts.interfaces.IBooster;
-import com.pixelmonmod.pixelmon.storage.NbtKeys;
+import com.pixelmonmod.pixelmon.api.storage.NbtKeys;
 import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.api.text.Text;
 
